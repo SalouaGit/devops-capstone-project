b3051037d8d9de04195842a2ca809dda68fb813f
SalouaGit <salouaelhiti@gmail.com>
2024-09-21 13:02:56 -0400

    Implemented, passed test case, and coverage checks for LIST accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,7 +123,7 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # TEST CASES FOR Read an account from the service
+    # TEST CASES FOR READING an account from the service
     def test_get_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
@@ -138,3 +138,14 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    # TEST CASES FOR LISTING accounts
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+
+    
