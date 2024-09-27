5bd4e1b377bef6bfe4761f0e75bca5bc03fd6637
SalouaGit <159430318+SalouaGit@users.noreply.github.com>
2024-09-27 16:09:32 +0100

    Edit test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,9 +22,7 @@ BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 
-######################################################################
-#  T E S T   C A S E S
-######################################################################
+# TEST CASES
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -60,8 +58,11 @@ class TestAccountService(TestCase):
         headers = {
             'X-Frame-Options': 'SAMEORIGIN',
             'X-Content-Type-Options': 'nosniff',
-            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
-            'Referrer-Policy': 'strict-origin-when-cross-origin'
+            'Content-Security-Policy': (
+                'default-src \'self\'; '
+                'object-src \'none\''
+            ),
+
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
@@ -71,13 +72,12 @@ class TestAccountService(TestCase):
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
-        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')                 
+        self.assertEqual(
+            response.headers.get('Access-Control-Allow-Origin'),
+            '*'
+        )
 
-
-    ######################################################################
-    #  H E L P E R   M E T H O D S
-    ######################################################################
-
+    # HELPER METHODES
     def _create_accounts(self, count):
         """Factory method to create accounts in bulk"""
         accounts = []
@@ -94,10 +94,7 @@ class TestAccountService(TestCase):
             accounts.append(account)
         return accounts
 
-    ######################################################################
-    #  A C C O U N T   T E S T   C A S E S
-    ######################################################################
-
+    # ACCOUNT TEST CASES
     def test_index(self):
         """It should get 200_OK from the Home Page"""
         response = self.client.get("/")
@@ -145,7 +142,10 @@ class TestAccountService(TestCase):
             json=account.serialize(),
             content_type="test/html"
         )
-        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
+        )
 
     # TEST CASES FOR READING an account from the service
     def test_get_account(self):
@@ -182,10 +182,13 @@ class TestAccountService(TestCase):
         # update the account
         new_account = resp.get_json()
         new_account["name"] = "Something Known"
-        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        resp = self.client.put(
+            f"{BASE_URL}/{new_account['id']}",
+            json=new_account
+        )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
-        self.assertEqual(updated_account["name"], "Something Known")    
+        self.assertEqual(updated_account["name"], "Something Known")
 
     # TEST CASES FOR DELETE accounts
     def test_delete_account(self):
@@ -193,7 +196,8 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
-  # Method Not Allowed
+
+    # Method Not Allowed
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
